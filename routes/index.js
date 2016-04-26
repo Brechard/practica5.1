5367a779201ef15e47763d888587fa56596f88d3
Rodrigo Brechard Alarcia <rodrigobrechard@gmail.com>
2016-04-26 14:58:05 +0200

    añadido página de créditos

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '5.1' });
+  res.render('index', { title: '5.1',direccion: __dirname });
 });
+router.get('/author', function (req,res,next) {
+	res.render('author',{nombre : 'Rodrigo Brechard Alarcia', 
+		foto : 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/4/005/039/09a/1d9adaf.jpg', 
+		video : 'https://www.youtube.com/embed/rrkrvAUbU9Y'})
+})
 
 module.exports = router;
